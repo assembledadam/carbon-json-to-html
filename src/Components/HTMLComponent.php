68327c9962187164d7e1994a20c055b638d551a2
Adam McCann <adam.mccann@videogamer.com>
2016-10-04 04:31:55 +0100

    Added class attribute to HTML components, with initial auto detection of item (only YouTube right now - need to add others - twitter, facebook etc)

--- src/Components/HTMLComponent.php
+++ src/Components/HTMLComponent.php
@@ -24,6 +24,15 @@ class HTMLComponent extends AbstractComponent implements ComponentInterface
     protected $customAttrs;
 
     /**
+     * Map for autodetection of URLs to determine type
+     *
+     * @var array
+     */
+    protected $autoDetectUrls = [
+        'youtube' => 'www.youtube.com/embed/',
+    ];
+
+    /**
      * Component constructor
      *
      * @param array|null
@@ -38,20 +47,46 @@ class HTMLComponent extends AbstractComponent implements ComponentInterface
      */
     public function parse(stdClass $json, DOMDocument $dom, DOMElement $parentElement)
     {
-        $container = $dom->createElement('figure');
+        $figure = $dom->createElement('figure');
 
         // create a temporary document and load the plain html
         $tmpDoc = new DOMDocument;
+        libxml_use_internal_errors(true); // for html5 tags
         $tmpDoc->loadHTML('<?xml encoding="UTF-8"><html><body>' . $json->html . '</body></html>');
         $tmpDoc->encoding = 'UTF-8';
+        libxml_clear_errors();
 
         // import and attach the created nodes to the paragraph
         foreach ($tmpDoc->getElementsByTagName('body')->item(0)->childNodes as $node) {
 
             $node = $dom->importNode($node, true);
-            $container->appendChild($node);
+            $figure->appendChild($node);
         }
 
-        return $parentElement->appendChild($container);
+        // try to autodetect type
+        $service = null;
+
+        foreach ($this->autoDetectUrls as $service => $url) {
+
+            if (strpos($json->html, $url) !== false) {
+                break;
+            }
+        }
+
+        // set figure classes
+        $class = 'embed-container html ';
+
+        if ($service) {
+            $class .= $service;
+        }
+
+        // if type is video and the html has an iframe, apply responsive fix class
+        if ($service == 'youtube' && strpos($json->html, '<iframe') !== false) {
+            $class .= ' responsive-video';
+        }
+
+        $figure->setAttribute('class', $class);
+
+        return $parentElement->appendChild($figure);
     }
 }
