c842866efcd7121cb4804920a31269650e555f27
Adam McCann <adam.mccann@videogamer.com>
2016-10-27 12:04:36 +0100

    Further fix to YouTube regex to allow for protocol-less schemas

--- src/Components/HTMLComponent.php
+++ src/Components/HTMLComponent.php
@@ -78,6 +78,8 @@ class HTMLComponent extends AbstractComponent implements ComponentInterface
         // create a temporary document and load the plain html
         $domElement = $this->loadHtml(trim($json->html));
 
+        dbg($json->html);
+
         // whether tags should be AMP compliant
         if (isset($this->config['amp'])) {
 
@@ -257,19 +259,19 @@ class HTMLComponent extends AbstractComponent implements ComponentInterface
     {
         $pattern =
             '%^# Match any youtube URL
-            (?:https?://)?  # Optional scheme. Either http or https
-            (?:www\.)?      # Optional www subdomain
-            (?:             # Group host alternatives
-              youtu\.be/    # Either youtu.be,
-            | youtube\.com  # or youtube.com
-              (?:           # Group path alternatives
-                /embed/     # Either /embed/
-              | /v/         # or /v/
-              | /watch\?v=  # or /watch\?v=
-              )             # End path alternatives.
-            )               # End host alternatives.
-            ([\w-]{10,12})  # Allow 10-12 for 11 char youtube id.
-            \??(.*)         # Allow query string
+            (?:(?:https?:)?//)? # Optional scheme. Either http or https
+            (?:www\.)?        # Optional www subdomain
+            (?:               # Group host alternatives
+              youtu\.be/      # Either youtu.be,
+            | youtube\.com    # or youtube.com
+              (?:             # Group path alternatives
+                /embed/       # Either /embed/
+              | /v/           # or /v/
+              | /watch\?v=    # or /watch\?v=
+              )               # End path alternatives.
+            )                 # End host alternatives.
+            ([\w-]{10,12})    # Allow 10-12 for 11 char youtube id.
+            \??(.*)           # Allow query string
             $%x';
 
         if (preg_match($pattern, $url, $matches)) {
